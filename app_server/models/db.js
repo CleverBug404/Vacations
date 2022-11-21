654e903ac048c7cdad41105a524a400b677008b8
JBlades10 <juan.velasquez@snhu.edu>
2022-11-20 23:12:27 -0500

    Connected Mongoose

---
+++ app_server/models/db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const readLine = require('readline');
+if (process.platform === 'win32'){
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', () => {
+        process.emit("SIGINT");
+    });
+}
+
+const dbURI = 'mongodb://127.0.0.1:27017/Loc8r';
+mongoose.connect(dbURI, {useNewUrlParser: true});
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        callback();
+    });
+};
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
+
+require('./locations');
+
+
+
