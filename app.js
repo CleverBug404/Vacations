4e2d8e7e336434837912d0667b98247f829df8c8
Juan Velasquez (SNHU) <79031419+JBlades10@users.noreply.github.com>
2022-12-25 21:40:57 -0500

    Update app.js
    Added inline comments' specifying the functions of the code

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-require('./app_server/models/db');
+const createError = require('http-errors'); // Import the http-errors module
+const express = require('express'); // Import the express module
+const path = require('path'); // Import the path module
+const cookieParser = require('cookie-parser'); // Import the cookie-parser module
+const logger = require('morgan'); // Import the morgan module
+require('./app_server/models/db'); // Import the db module
 
-const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
+const indexRouter = require('./app_server/routes/index'); // Import the index route
+const usersRouter = require('./app_server/routes/users'); // Import the users route
 
-const app = express();
+const app = express(); // Create an instance of the express app
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'app_server', 'views')); // Set the views directory
+app.set('view engine', 'pug'); // Set the view engine to pug
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev')); // Use the logger middleware in 'dev' mode
+app.use(express.json()); // Use the JSON parser middleware
+app.use(express.urlencoded({ extended: false })); // Use the URL encoder middleware
+app.use(cookieParser()); // Use the cookie parser middleware
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
 
-app.use('/', indexRouter);
+app.use('/', indexRouter); // Use the index route
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
